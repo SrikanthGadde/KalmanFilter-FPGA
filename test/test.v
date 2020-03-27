86988937a20402e1023255d178017a6b31845eda
SrikanthGadde <srikanth160247@mechyd.ac.in>
2020-03-28 00:34:24 +0530

    All blocking assignment
    TODO: Convert to non-blocking assignment for sequential logic

--- test/test.v
+++ test/test.v
@@ -8,22 +8,23 @@ module test(num, clk, rst ,sqrt);
     reg [17:0] left,right,r;    
     reg [4:0] i;
 	always@(posedge clk)
-	begin
+	
 	if(rst ==  1'b1)
 	begin
-    a <= num;
+    a = num;
     q = 0;
-    i <= 0;
-    left <= 0;   //input to adder/sub
-    right <= 0;  //input to adder/sub
-    r <= 0;  //remainder
+    i = 0;
+    left = 0;   //input to adder/sub
+    right = 0;  //input to adder/sub
+    r = 0;  //remainder
     //run the calculations for 16 iterations.
-	 i <= 'b0;
+	 i = 'b0;
 	  end
 	  else
-	  begin
+	  
 	 casez (i)
 		5'b0????: begin 
+			i = i + 'b1;
         right = {q,r[17],1'b1};
         left = {r[15:0],a[31:30]};
         a = {a[29:0],2'b00};    //left shift by 2 bits.
@@ -32,11 +33,12 @@ module test(num, clk, rst ,sqrt);
         else    //subtract if r is positive
             r = left - right;
         q = {q[14:0],!r[17]};   
-		  i = i + 'b1;
 		  end
+		5'b1????:
+			sqrt = q;   //final assignment of output.
 		  
-	endcase	
-		end
-    sqrt = q;   //final assignment of output.
-	 end
+		endcase	
+		
+    
+	 
 endmodule
